a7761d49d5e14eecdf4f4ca000cfd6df38eac2c3
zydric <zydric.abel@cit.edu>
2026-03-07 08:48:47 +0800

    feat: add UserRepository with email lookup methods

---
+++ backend/src/main/java/edu/cit/abel/washq/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package edu.cit.abel.washq.repository;
+
+import edu.cit.abel.washq.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
