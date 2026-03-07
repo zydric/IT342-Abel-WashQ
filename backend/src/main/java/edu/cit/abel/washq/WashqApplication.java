c0f5610cb3a76cb4e16aabc5ad47ac582e6c6fee
zydric <zydric.abel@cit.edu>
2026-03-07 08:19:51 +0800

    feat: setup spring backend

---
+++ backend/src/main/java/edu/cit/abel/washq/WashqApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.abel.washq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashqApplication.class, args);
+	}
+
+}
